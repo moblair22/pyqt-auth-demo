a417237dde3dae8e2e782cfe635b899b08b05615
moblair22 <130151188+moblair22@users.noreply.github.com>
2026-08-22 11:42:22 -0400

    Add Goal Command Center service worker

---
+++ goal-command-center/sw.js
@@ -0,0 +1,5 @@
+const CACHE='goal-command-center-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
